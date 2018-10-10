2cc0cfdf2ccb6a3502a6d49e9f1df5bfdeb53ed7
nabettu <tatsuaki.w@gmail.com>
2018-10-10 12:10:08 +0900

    error解消

--- src/pages/auth.js
+++ src/pages/auth.js
@@ -5,23 +5,34 @@ import Layout from '../components/layout'
 
 const SecondPage = () => (
   <Layout>
-  <script>
-    $(function() {
-      $("#login").on("click", function(e) {
-        e.preventDefault();
-        var authenticator = new netlify.default ({});
-        authenticator.authenticate({provider:"github", scope: "user"}, function(err, data) {
-          if (err) {
-            return $("#output").text("Error Authenticating with GitHub: " + err);
+    <script>
+      $(function()
+      {$('#login').on('click', function(e) {
+        e.preventDefault()
+        var authenticator = new netlify.default({})
+        authenticator.authenticate(
+          { provider: 'github', scope: 'user' },
+          function(err, data) {
+            if (err) {
+              return $('#output').text(
+                'Error Authenticating with GitHub: ' + err
+              )
+            }
+            $('#output').text(
+              'Authenticated with GitHub. Access Token: ' + data.token
+            )
           }
-          $("#output").text("Authenticated with GitHub. Access Token: " + data.token);
-        });
-      });
-    });
-  </script>
-  <h1>GitHub Auth Demo:</h1>
-  <p><a href="#" id="login">Authenticate</a></p>
-  <p id="output"></p>
+        )
+      })}
+      );
+    </script>
+    <h1>GitHub Auth Demo:</h1>
+    <p>
+      <a href="#" id="login">
+        Authenticate
+      </a>
+    </p>
+    <p id="output" />
   </Layout>
 )
 
